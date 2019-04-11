e2770e18e367a6f63adb24e13c26dc599a809667
Erik Olby <erik.olby@gmail.com>
2019-04-11 14:24:26 +0200

    Added Pilon bash script

---
+++ pilon_run_script.sh
@@ -0,0 +1,17 @@
+!/bin/bash -l
+
+#SBATCH -A g2019003
+#SBATCH -p core
+#SBATCH -n 10
+#SBATCH -t 00:15:00
+#SBATCH -J genome_pilon_correction_erik_olby
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user erik.olby@gmail.com
+
+# Load modules
+module load bioinfo-tools
+module load Pilon/1.22
+
+
+# Commands
+pilon pilon --genome /home/erol7379/genome_analysis/analyses/01_pac_bio_assembly_outfiles/genome_assembly.contigs.fasta --bam /home/erol7379/genome_analysis/analyses/02_assembly_correction_pilon/bwa_alignment.sorted.bam --diploid --threads 2 --output pilon_output
