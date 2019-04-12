372e6ed7ab7c86a63079a972c195717d8e962b8e
Erik Olby <erik.olby@gmail.com>
2019-04-12 15:29:50 +0200

    Wrote a tophat script

---
+++ tophat_script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -l
+
+#SBATCH -A g2019003
+#SBATCH -p core
+#SBATCH -n 4
+#SBATCH -t 10:00:00
+#SBATCH -J tophat_transcriptome_erik_olby
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user erik.olby@gmail.com
+
+# Load modules
+module load bioinfo-tools
+module load samtools 
+module load bowtie2
+module load tophat
+
+
+# Commands
+for file in /home/erol7379/genome_analysis/data/raw_data/transcriptome/trimmed/*_10.1.fastq.gz
+do 
+	file2="$(sed 's/_10.1/_10.2' <<<$file)"
+
+	tophat -o /home/erol7379/genome_analysis/analyses/04_tophat_transcript_mapping/outputfiles \
+	-p 4 /home/erol7379/genome_analysis/analyses/04_tophat_transcript_mapping/bowtie_index_output $file $file2
