9bd24d39528b1a673e8ec3a22a35ee4a5a74a2a0
Erik Olby <erik.olby@gmail.com>
2019-04-25 11:09:15 +0200

    fixed the output of the trinity script

--- trinity_script.sh
+++ trinity_script.sh
@@ -18,5 +18,5 @@ module load trinity
 for file in /home/erol7379/genome_analysis/analyses/04_tophat_transcript_mapping/outputfiles_sorted/*	
 do 
 	output=$(basename $file)
-	Trinity --genome_guided_bam $file --genome_guided_max_intron 10000 --max_memory 20G --CPU 4 --output /home/erol7379/genome_analysis/analyses/04_tophat_transcript_mapping/trinity_outfiles
+	Trinity --genome_guided_bam $file --genome_guided_max_intron 10000 --max_memory 20G --CPU 4 --output /home/erol7379/genome_analysis/analyses/04_tophat_transcript_mapping/trinity_outfiles_${output%.bam}
 done
