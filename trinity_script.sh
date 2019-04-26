0402ae5be22b202ff9adb0086c017bfc269dca62
Erik Olby <erik.olby@gmail.com>
2019-04-26 15:51:11 +0200

    Changed the max memory and the number of cores

--- trinity_script.sh
+++ trinity_script.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -A g2019003
 #SBATCH -p core
-#SBATCH -n 6
+#SBATCH -n 4
 #SBATCH -t 15:00:00
 #SBATCH -J transcrpit_assembly_trinity_erik_olby
 #SBATCH --mail-type=ALL
@@ -17,5 +17,5 @@ module load trinity
 for file in /home/erol7379/genome_analysis/analyses/04_tophat_transcript_mapping/outputfiles_sorted/*.bam	
 do 
 	output=$(basename $file)
-	Trinity --genome_guided_bam $file --genome_guided_max_intron 10000 --max_memory 20G --CPU 6 --output /home/erol7379/genome_analysis/analyses/04_tophat_transcript_mapping/trinity_outfiles_${output%.bam}
+	Trinity --genome_guided_bam $file --genome_guided_max_intron 10000 --max_memory 10G --CPU 4 --output /home/erol7379/genome_analysis/analyses/04_tophat_transcript_mapping/trinity_outfiles_${output%.bam}
 done
