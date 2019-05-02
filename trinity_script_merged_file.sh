762b9dd2d37d837e191beb4ee8441cf5f03a164a
Erik Olby <erik.olby@gmail.com>
2019-05-02 14:33:22 +0200

    wrote a trinity script for the merged outfile

---
+++ trinity_script_merged_file.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+
+#SBATCH -A g2019003
+#SBATCH -p core
+#SBATCH -n 4
+#SBATCH -t 15:00:00
+#SBATCH -J transcrpit_assembly_trinity_erik_olby
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user erik.olby@gmail.com
+
+# Load modules
+module load bioinfo-tools
+module load trinity
+
+Trinity --genome_guided_bam $file --genome_guided_max_intron 10000 --max_memory 10G --CPU 4 --output /home/erol7379/genome_analysis/analyses/04_tophat_transcript_mapping/trinity_outfiles
