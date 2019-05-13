690673dfcaee6353cb294cb68245d8a0beae4bbe
Erik Olby <erik.olby@gmail.com>
2019-05-13 14:54:46 +0200

    Sorted and merged different files which I later run trinity on

--- trinity_script_merged_file.sh
+++ trinity_script_merged_file.sh
@@ -3,7 +3,7 @@
 #SBATCH -A g2019003
 #SBATCH -p core
 #SBATCH -n 4
-#SBATCH -t 10:00:00
+#SBATCH -t 5:00:00
 #SBATCH -J transcrpit_assembly_trinity_erik_olby
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user erik.olby@gmail.com
@@ -12,4 +12,4 @@
 module load bioinfo-tools
 module load trinity
 
-Trinity --genome_guided_bam /home/erol7379/genome_analysis/analyses/04_tophat_transcript_mapping/merged_sorted_outfile/merged_sorted.bam --genome_guided_max_intron 10000 --max_memory 20G --CPU 4 --output /home/erol7379/genome_analysis/analyses/04_tophat_transcript_mapping/trinity_outfiles --full_cleanup
+Trinity --genome_guided_bam /home/erol7379/genome_analysis/analyses/04_tophat_transcript_mapping/merged_sorted_outfile/merged_sorted_outfile.bam --genome_guided_max_intron 10000 --max_memory 20G --CPU 4 --output /home/erol7379/genome_analysis/analyses/04_tophat_transcript_mapping/trinity_outfiles
