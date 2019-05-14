6726125aab6f2ffc4a502c5d0c5c5fe2a3783912
Erik Olby <erik.olby@gmail.com>
2019-05-14 17:29:14 +0200

    Wrote a script for running maker

---
+++ maker_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+
+#SBATCH -A g2019003
+#SBATCH -p core
+#SBATCH -n 4
+#SBATCH -t 15:00:00
+#SBATCH -J maker_annotation_erik_olby
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user erik.olby@gmail.com
+
+# Load modules
+module load bioinfo-tools
+module load maker 
+
+cd /home/erol7379/genome_analysis/analyses/06_annotation/maker_runs
+maker 
