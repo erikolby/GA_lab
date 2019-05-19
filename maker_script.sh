27bf2f1cc85f0813e58a91547a888acf87898de5
Erik Olby <erik.olby@gmail.com>
2019-05-19 23:09:51 +0200

    Changed time

--- maker_script.sh
+++ maker_script.sh
@@ -3,18 +3,17 @@
 #SBATCH -A g2019003
 #SBATCH -p core
 #SBATCH -n 4
-#SBATCH -t 00:10:00
+#SBATCH -t 15:00:00
 #SBATCH -J maker_annotation_erik_olby
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user erik.olby@gmail.com
 
 # Load modules
 module load bioinfo-tools
-module load maker/3.01.1-beta
+module load maker
 
 cd /home/erol7379/genome_analysis/analyses/06_annotation/maker_runs
 
-# Undrar: Får error nu att den inte gittar min my_genome.hmm fil som ligger i maker_runs/pilon_blabla/, men den ligger där, och allt annat verkar fungera?
 
 maker 
 
