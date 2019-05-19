239a58a08174b9d18f30e507a6554d0bd75e985c
Erik Olby <erik.olby@gmail.com>
2019-05-19 23:04:45 +0200

    Changed the loading module

--- maker_script.sh
+++ maker_script.sh
@@ -10,9 +10,11 @@
 
 # Load modules
 module load bioinfo-tools
-module load maker
+module load maker/3.01.1-beta
 
 cd /home/erol7379/genome_analysis/analyses/06_annotation/maker_runs
+
+# Undrar: Får error nu att den inte gittar min my_genome.hmm fil som ligger i maker_runs/pilon_blabla/, men den ligger där, och allt annat verkar fungera?
 
 maker 
 
