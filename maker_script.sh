078006182b75e07a643345935014d2818b4b3982
Erik Olby <erik.olby@gmail.com>
2019-05-20 16:32:54 +0200

    Changed the time of the script

--- maker_script.sh
+++ maker_script.sh
@@ -3,7 +3,7 @@
 #SBATCH -A g2019003
 #SBATCH -p core
 #SBATCH -n 4
-#SBATCH -t 15:00:00
+#SBATCH -t 13:00:00
 #SBATCH -J maker_annotation_erik_olby
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user erik.olby@gmail.com
