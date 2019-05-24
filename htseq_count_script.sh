839c9f9b439a9458b79afcce70141ca662eff904
Erik Olby <erik.olby@gmail.com>
2019-05-24 14:27:55 +0200

    syntax

--- htseq_count_script.sh
+++ htseq_count_script.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 for file in /home/erol7379/genome_analysis/analyses/07_new_files_prot_assem/tophat_sorted/used_files/*
-output=$(basename $file)
-htseq-count -f bam -r pos -i ID -o /home/erol7379/genome_analysis/analyses/07_new_files_prot_assem/htseq_count_output/test_$output.sam $file /home/erol7379/genome_analysis/analyses/07_new_files_prot_assem/ref_genome_annotated.gff3
+do
+	output=$(basename $file)
+	htseq-count -f bam -r pos -i ID -o /home/erol7379/genome_analysis/analyses/07_new_files_prot_assem/htseq_count_output/test_$output.sam $file /home/erol7379/genome_analysis/analyses/07_new_files_prot_assem/ref_genome_annotated.gff3
 done
 
