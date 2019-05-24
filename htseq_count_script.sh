06f701a934c6d7a07ae5ed660116047ec1ade100
Erik Olby <erik.olby@gmail.com>
2019-05-24 17:32:03 +0200

    wrote a script

--- htseq_count_script.sh
+++ htseq_count_script.sh
@@ -1,7 +1,17 @@
-#!/bin/bash
-for file in /home/erol7379/genome_analysis/analyses/07_new_files_prot_assem/tophat_sorted/used_files/*
-do
-	output=$(basename $file)
-	htseq-count -f bam -r pos $file /home/erol7379/genome_analysis/analyses/07_new_files_prot_assem/ref_genome_annotation_altered.gtf -o /home/erol7379/genome_analysis/analyses/07_new_files_prot_assem/htseq_count_output/htseq_count_$output.sam
-done
+#!/bin/bash -l
 
+#SBATCH -A g2019003
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 2:00:00
+#SBATCH -J htseq_count_erik_olby
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user erik.olby@gmail.com
+
+# Load modules
+module load bioinfo-tools
+module load htseq
+
+htseq-count --format='bam' --order='name' --stranded='yes' --type='exon' --idattr='gene_id' --samout='htseq_output' /home/erol7379/genome_analysis/analyses/07_new_files_prot_assem/tophat_sorted/used_files/accepted_hits_sorted_2019-05-24_tophat_out_SRR6040093_scaffold_10.1.bam ref_genome_annotation_altered.gtf > htseq_out93.txt
+htseq-count --format='bam' --order='name' --stranded='yes' --type='exon' --idattr='gene_id' --samout='htseq_output' /home/erol7379/genome_analysis/analyses/07_new_files_prot_assem/tophat_sorted/used_files/accepted_hits_sorted_2019-05-24_tophat_out_SRR6040096_scaffold_10.1.bam ref_genome_annotation_altered.gtf > htseq_out96.txt
+htseq-count --format='bam' --order='name' --stranded='yes' --type='exon' --idattr='gene_id' --samout='htseq_output' /home/erol7379/genome_analysis/analyses/07_new_files_prot_assem/tophat_sorted/used_files/accepted_hits_sorted_2019-05-24_tophat_out_SRR6040097_scaffold_10.1.bam ref_genome_annotation_altered.gtf > htseq_out97.txt
